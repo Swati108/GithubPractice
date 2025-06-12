ee51b18da6def603f27762275d63aae0b8ed3a56
Swati108 <136457414+Swati108@users.noreply.github.com>
2025-06-12 11:19:17 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -5,7 +5,7 @@ import os
 
 def initialize_rag_pipeline(doc_dir="data/sample_docs", rebuild=False):
     if rebuild or not os.path.exists("faiss_index"):
-        print("Loading and processing local ")
+        print("Loading and processing Demo github ")
         documents = load_and_split_documents(doc_dir)
         print("Creating vector store document 1")
         create_vector_store(documents)
